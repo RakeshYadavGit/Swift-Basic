f00a4dba6563e33a209a307aef749a17e7396a27
RakeshYadavGit <85060648+RakeshYadavGit@users.noreply.github.com>
2023-06-09 07:10:36 +0000

    Learn about data types in swift

---
+++ Basic/UseOfDataTypes.swift
@@ -0,0 +1,19 @@
+let userName:String = "Rakesh Yadav"
+let userDiv:Character = "A"
+let userRollNo:Int = 63
+let userPercentage:Double = 90.339393939
+let floatType:Float = 12.123456
+
+print("User Name is \(userName)")
+print("User Div is \(userDiv)")
+print("User Roll Number is \(userRollNo)")
+print("User Percentage is \(userPercentage)")
+print("floatType value is \(floatType)")
+
+/*output
+User Name is Rakesh Yadav
+User Div is A
+User Roll Number is 63
+User Percentage is 90.339393939
+floatType value is 12.123456
+*/
