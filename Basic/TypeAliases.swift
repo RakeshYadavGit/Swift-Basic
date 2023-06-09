a5d00eafc2ec3f3928a752e8d33cf74a60e1eeb8
RakeshYadavGit <85060648+RakeshYadavGit@users.noreply.github.com>
2023-06-09 07:01:07 +0000

    Learn about Type Aliases

---
+++ Basic/TypeAliases.swift
@@ -0,0 +1,18 @@
+typealias EmployeeID = Int
+
+let userID: EmployeeID = 12345
+print("User Id is \(userID) \n Type of userID is \(type(of:userID))")
+
+/*output
+User Id is 12345 
+Type of userID is Int
+*/
+
+typealias EmployeeName = String
+let userName: EmployeeName = "RAKESH YADAV"
+print("User Name is \(userName) \n Type of userName is \(type(of:userName))")
+
+/*output
+User Name is RAKESH YADAV 
+Type of userName is String
+*/
